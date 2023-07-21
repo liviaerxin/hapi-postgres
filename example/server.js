9b4a9885c971d86927ce7399a632718142642711
frank chen <frankchen@astri.org>
2023-07-21 14:17:21 +0800

    update 3 files and create 1 file

--- example/server.js
+++ example/server.js
@@ -3,33 +3,50 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-  const server = Hapi.server({
-    port: 4000,
-    host: '0.0.0.0',
-  });
+    const server = Hapi.server({
+        port: 4000,
+        host: '0.0.0.0',
+        debug: { log: ['hapi-postgresql'] },
+    });
 
-  await server.register({
-    plugin: require('../lib'),
-    options: {
-      name: 'Bob1111',
-    },
-  });
+    await server.register({
+        plugin: require('../lib'),
+        options: {
+            settings: {
+                connectionString: 'postgresql://postgres:123456@127.0.0.1/db',
+                max: 10,
+                connectionTimeoutMillis: 1000,
+            },
+            decorate: true,
+        },
+    });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return 'Hello World!';
-    },
-  });
+    server.route({
+        method: 'GET',
+        path: '/test',
+        async handler(request) {
+            const pool = request.pg.pool;
+            console.log(pool);
 
-  await server.start();
-  console.log('Server running on %s', server.info.uri);
+            try {
+                const { fields, rows } = await pool.query('SELECT NOW() as now');
+                console.error('fields', fields);
+                console.error('rows', rows);
+
+                return { fields, rows };
+            } catch (err) {
+                console.error('pg query', err);
+            }
+        },
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', err => {
-  console.log(err);
-  process.exit(1);
+    console.log(err);
+    process.exit(1);
 });
 
 init();
